da5a1f59ceedfab8665acb15cf651532a3d52f8b
SebastianSzt <94396040+SebastianSzt@users.noreply.github.com>
2022-10-24 09:45:10 +0200

    Dodanie rozwiązania zadania 1

--- zadania1-5/lib.c
+++ zadania1-5/lib.c
@@ -1,2 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lib.h"
 
+int main(int argc, char** argv)
+{
+    node* head = NULL;
+    int select = 1;
 
+    while (select)
+    {
+        printf("Menu naszego programu do list. Wybierz odpowiedniuą opcję.\n");
+        printf("1 - dodaj na początek listy\n");
+        printf("2 - dodaj na koniec listy\n");
+        printf("Wybierz opcję: ");
+        scanf("%d", &select);
+        switch (select)
+        {
+            case 1:
+                //push();
+                break;
+            case 2:
+                //pushEnd();
+                break;
+            default:
+                break;
+        }
+    }
+}
