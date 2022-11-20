c73bb1ef2e88c44ee869e42fb8688dd96aa9d03a
Sebastian Sztankowski <313061@stud.umk.pl>
2022-11-21 00:33:57 +0100

    Aktualizacja 21.11

--- zadania1-5/lib.c
+++ zadania1-5/lib.c
@@ -7,10 +7,10 @@ void WyswietlListe (node *head)
     node *l = head;
     while (l)
     {
-        printf ("%d-", l->data);
+        printf ("%d->", l->data);
         l = l->next;
     }
-    printf ("|\n");
+    printf ("\n");
 }
 
 void push(node **head, int val)
@@ -31,4 +31,174 @@ void pushEnd(node **head, int val)
         head = &(*head)->next;
     }
     *head = p;
+}
+
+void pop(node **head)
+{
+    node *l = *head;
+    if(l)
+    {
+        *head = l->next;
+        free(l);
+    }
+}
+
+void popEnd(node **head)
+{
+    node *l = *head;
+    if(l)
+    {
+        if ( l->next != NULL)
+        {
+            while(l->next->next)
+            {
+                l = l->next;
+            }
+            free(l->next);
+            l->next = NULL;
+        }
+        else
+        {
+            free(l);
+            *head = NULL;
+        }
+    }
+}
+
+node* find(node *head, int val)
+{
+    while ((head != NULL) && (head->data != val))
+    {
+        head = head->next;
+    }
+    return head;
+}
+
+void pushBefore(node **head, node *current, int val)
+{
+    node *p = *head;
+    if( p == current )
+    {
+        push(head, val);
+    }
+    else
+    {
+        while( p->next != current )
+        {
+            p = p->next;
+        }
+        p->next = (node*)malloc(sizeof(node));
+        p->next->next = current;
+        p->next->data = val;
+    }
+}
+
+void pushAfter(node **head, node *current, int val)
+{
+    node *p = *head;
+    while( p != current )
+    {
+        p = p->next;
+    }
+    node *end = (node*)malloc(sizeof(node));
+    end->data = val;
+    end->next = current->next;
+    p->next = end;
+}
+
+void removeNode(node **head, node *current)
+{
+    node *p = *head;
+    if( p == current )
+    {
+        pop(head);
+    }
+    else
+    {
+        while( p->next != current ) 
+        {
+            p = p->next;
+        }
+        free(p->next);
+        p->next = NULL;
+        p->next = current->next;
+    }
+}
+
+node* readFromFile(char *fname)
+{
+    FILE *file = fopen(fname, "r");
+    node* head = NULL;
+    if (file == NULL)
+    {
+        printf("Error.\n");
+    }
+    else
+    {
+        int val;
+        while(fscanf(file, "%d ",&val) > 0)
+        {
+            pushEnd(&head, val);
+        }
+    }
+    fclose(file);
+    return head;
+}
+
+int saveToFile(node *head, char *fname)
+{
+    FILE *file = fopen(fname, "w");
+    if (file == NULL)
+    {
+        printf("Error.\n");
+        fclose(file);
+        return 0;
+    }
+    else
+    {
+        for (node* curr = head; curr != NULL; curr = curr->next)
+        {
+            fprintf(file, "%d ", curr->data);
+        }
+        fclose(file);
+        return 1;
+    }
+}
+
+void removeAll_iteratively(node **head, int val)
+{
+    node* current;
+    do
+    {
+        current = find(*head, val);
+        removeNode(head, current);
+    }
+    while(current != NULL);
+}
+
+node* revert_iteratively(node *head)
+{
+    node *new = NULL;
+    node *tmp;
+    while (head != NULL)
+    {
+        tmp = head->next;
+        head->next = new;
+        new = head;
+        head = tmp;
+    }
+    head = new;
+    return head;
+}
+
+void revert_recursively(node *head)
+{
+    node *p = head;
+    if ((head != NULL) && (head->next != NULL))
+    {
+        head = head->next;
+        revert_recursively(head);
+        p->next->next = p;
+        p->next = NULL;
+    }
 }
--- zadania1-5/main.c
+++ zadania1-5/main.c
@@ -9,11 +9,23 @@ int main(int argc, char** argv)
 
     while (select)
     {
+        //1,2,5,6
         printf("Menu programu do list. Wybierz odpowiednią opcję.\n");
         printf("0 - Wyjscie z programu\n");
-        printf("1 - dodaj element na poczatku listy\n");
-        printf("2 - dodaj element na koncu listy\n");
-        printf("3 - usun pierwszy element listy\n");
+        printf("1 - dodawanie na początek listy\n");
+        printf("2 - dodawanie na koniec listy\n");
+        printf("3 - usuwanie pierwszego elementu listy\n");
+        printf("4 - usuwanie ostatniego elementu listy\n");
+        printf("5 - odszukaj zadany element\n");
+        printf("6 - dodaj element przed wskazanym\n");
+        printf("7 - dodaj za wskazanym elementem\n");
+        printf("8 - usuń wskazany element\n");
+        printf("9 - wczytaj zawartość listy z pliku\n");
+        printf("10 - zapisz zawartość listy do pliku\n");
+        printf("11 - usuń wszystkie wartości val z listy iteracynie\n");
+        printf("12 - usuń wszystkie wartości val z listy rekurencyjnie\n");
+        printf("13 - odwróć listę iteracyjnie\n");
+        printf("14 - odwróć listę rekurencyjnie\n");
         printf("Wybierz opcję: ");
         scanf("%d", &select);
         switch (select)
@@ -35,8 +47,148 @@ int main(int argc, char** argv)
                 pushEnd(&head, 3);
                 WyswietlListe(head);
                 break;
+            case 3:
+                push(&head, 3);
+                push(&head, 2);
+                push(&head, 1);
+                printf("Lista przed usunieciem pierwszego elementu:\n");
+                WyswietlListe(head);
+                pop(&head);
+                printf("Lista po usunieciu pierwszego elementu:\n");
+                WyswietlListe(head);
+                break;
+            case 4:
+                push(&head, 3);
+                push(&head, 2);
+                push(&head, 1);
+                printf("Lista przed usunieciem ostatniego elementu:\n");
+                WyswietlListe(head);
+                popEnd(&head);
+                printf("Lista po usunieciu ostatniego elementu:\n");
+                WyswietlListe(head);
+                break;
+            case 5:
+            {
+                push(&head, 3);
+                push(&head, 2);
+                push(&head, 1);
+                push(&head->next, 0);
+                push(&head, 10);
+                printf("Lista:\n");
+                WyswietlListe(head);
+                node* current = find(head, 0);
+                printf("Lista szukanego elementu:\n");
+                WyswietlListe(current);
+                break;
+            }
+            case 6:
+            {
+                push(&head, 3);
+                push(&head, 2);
+                push(&head, 1);
+                push(&head->next, 0);
+                push(&head, 10);
+                printf("Lista przed:\n");
+                WyswietlListe(head);
+                node* current = find(head, 0);
+                pushBefore(&head, current, 100);
+                printf("Lista po:\n");
+                WyswietlListe(head);
+                break;
+            }
+            case 7:
+            {
+                push(&head, 3);
+                push(&head, 2);
+                push(&head, 1);
+                push(&head->next, 0);
+                push(&head, 10);
+                printf("Lista przed:\n");
+                WyswietlListe(head);
+                node* current = find(head, 0);
+                pushAfter(&head, current, 100);
+                printf("Lista po:\n");
+                WyswietlListe(head);
+                break;
+            }
+            case 8:
+            {
+                push(&head, 3);
+                push(&head, 2);
+                push(&head, 1);
+                push(&head->next, 0);
+                push(&head, 10);
+                printf("Lista przed:\n");
+                WyswietlListe(head);
+                node* current = find(head, 0);
+                removeNode(&head, current);
+                printf("Lista po:\n");
+                WyswietlListe(head);
+                break;
+            }
+            case 9:
+            {
+                head = readFromFile("plik.txt");
+                WyswietlListe(head);
+                break;
+            }
+            case 10:
+            {
+                head = readFromFile("plik.txt");
+                pushEnd(&head, 1);
+                pushEnd(&head, 2);
+                pushEnd(&head, 3);
+                int test;
+                test = saveToFile(head, "plik.txt");
+                if (test == 1) printf("Zapis do pliku powiodl sie.\n");
+                else if (test == 0) printf("Zapis do pliku nie powiodl sie.\n");
+                break;
+            }
+            case 11:
+            {
+                head = readFromFile("plik.txt");
+                printf("Lista przed:\n");
+                WyswietlListe(head);
+                removeAll_iteratively(&head, 2);
+                printf("Lista po:\n");
+                WyswietlListe(head);
+                break;
+            }
+            case 12:
+            {
+                // head = readFromFile("plik.txt");
+                // printf("Lista przed:\n");
+                // WyswietlListe(head);
+                // removeAll_recursively(&head, 2);
+                // printf("Lista po:\n");
+                // WyswietlListe(head);
+                break;
+            }
+            case 13:
+            {
+                head = readFromFile("plik.txt");
+                printf("Lista przed:\n");
+                WyswietlListe(head);
+                node* revert;
+                revert = revert_iteratively(head);
+                printf("Lista po:\n");
+                WyswietlListe(revert);
+                break;
+            }
+            case 14:
+            {
+                head = readFromFile("plik.txt");
+                printf("Lista przed:\n");
+                WyswietlListe(head);
+                revert_recursively(head);
+                printf("Lista po:\n");
+                WyswietlListe(head);
+                break;
+            }
             default:
+                printf("Niepoprawna cyfra.\n");
                 break;
         }
+        exit(0);
     }
 }
